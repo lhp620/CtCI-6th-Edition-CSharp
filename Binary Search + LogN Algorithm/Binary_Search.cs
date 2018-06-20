eb4bf593fe868481b0ac4c51cff1b634912ff759
v-huli <v-huli@HUAPING-THINK>
2018-06-20 01:06:15 -0700

    adding for Jiuzhang

---
+++ Binary Search + LogN Algorithm/Binary_Search.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ctci.Contracts;
+
+namespace Jiuzhang_02
+{
+    class Binary_Search : IQuestion
+    {
+
+
+        public void Run()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
