410ac3fcca9d95de7c8c9099f32e2ac46b93625c
Huaping Liu (CSI Interfusion Inc) <v-huli@microsoft.com>
2018-07-09 18:40:37 -0700

    update

---
+++ JiuZhangTest/UnitTest7.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using JiuZhang.Chapter7;
+
+namespace JiuZhang.Test
+{
+    [TestClass]
+    public class UnitTest7
+    {
+        [TestMethod]
+        public void String_Permutation_test()
+        {
+            var array = "ABC";
+            String_Permutation.Permutate(array, 0, array.Length -1);
+
+            Assert.IsTrue(true);
+        }
+    }
+}
