6ebdcaf4113372856c830b80a9fac3fbc7c3c98d
v-huli <v-huli@HUAPING-THINK>
2018-10-15 22:41:21 -0700

    update

--- Tutorialhorizon_Practices/Dynamic Programming – Coin Change Problem.cs
+++ Tutorialhorizon_Practices/Dynamic Programming – Coin Change Problem.cs
@@ -28,5 +28,45 @@ namespace Tutorialhorizon_Practices
 
             return total(n - v[i], v, i) + total(n, v, i + 1);  
         }
+
+        public static int totalDynamic(int[] v, int amount)
+        {
+            int[][] solution = new int[v.Length + 1][];
+            for(int i = 0; i < solution.Length; i++)
+            {
+                solution[i] = new int[amount + 1];
+            }
+
+            // if amount = 0, then just return empty set to make the change
+            for (int i = 0; i<=v.Length; i++)
+            {
+                solution[i][0] = 1;
+            }
+
+            // if no coin given, 0 ways to change the amount
+            for (int i = 0; i < amount; i++)
+            {
+                solution[0][i] = 0;
+            }
+
+            // now fill the rest of the matrix
+            for (int i = 1; i <=v.Length; i++)
+            {
+                for(int j = 1; j<=amount; j++)
+                {
+                    // check if the coin value is less than the amount needed
+                    if (v[i-1] <=j)
+                    {
+                        solution[i][j] = solution[i - 1][j] + solution[i][j - v[i - 1]];
+                    }
+                    else
+                    {
+                        solution[i][j] = solution[i - 1][j];
+                    }
+                }
+            }
+            return solution[v.Length][amount];
+        }
+
     }
 }
---
+++ Tutorialhorizon_Practices/Dynamic Programming – Minimum Cost Path Problem.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tutorialhorizon_Practices
+{
+    class Dynamic_Programming___Minimum_Cost_Path_Problem
+    {
+        public static int find(int[][] A)
+        {
+            int[][] solution = new int[A.Length][];
+            for(int i = 0; i < A.Length; i++)
+            {
+                solution[i] = new int[A.Length];
+            }
+
+            solution[0][0] = A[0][0];
+
+            for (int i = 1; i < A.Length; i++)
+            {
+                solution[0][i] = A[0][i] + solution[0][i - 1];
+            }
+
+            for (int i = 1; i < A.Length; i++)
+            {
+                solution[i][0] = A[i][0] + solution[i - 1][0];
+            }
+
+            // path will be either from top or left, choose which ever is minimum
+            for (int i = 1; i < A.Length;i++)
+            {
+                for (int j = 1; j < A.Length; j++)
+                {
+                    solution[i][j] = A[i][j] + Math.Min(solution[i - 1][j], solution[i][j - 1]);
+                }
+            }
+
+            return solution[A.Length - 1][A.Length - 1];
+        }
+    }
+}
