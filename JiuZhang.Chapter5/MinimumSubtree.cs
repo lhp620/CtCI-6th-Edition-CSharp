334c30c541c22ad7f1707c7c1773996f224d888c
v-huli <v-huli@HUAPING-THINK>
2019-09-10 23:21:34 -0700

    add new code

---
+++ JiuZhang.Chapter5/MinimumSubtree.cs
@@ -0,0 +1,40 @@
+﻿using ctci.Library;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JiuZhang.Chapter5
+{
+    class MinimumSubtree
+    {
+        private TreeNode subtree = null;
+        private int subtreeSum = Int32.MaxValue;
+        /**
+         * @param root the root of binary tree
+         * @return the root of the minimum subtree
+         */
+        public TreeNode findSubtree(TreeNode root)
+        {
+            helper(root);
+            return subtree;
+        }
+
+        private int helper(TreeNode root)
+        {
+            if (root == null)
+            {
+                return 0;
+            }
+
+            int sum = helper(root.Left) + helper(root.Right) + root.Data;
+            if (sum <= subtreeSum)
+            {
+                subtreeSum = sum;
+                subtree = root;
+            }
+            return sum;
+        }
+    }
+}
