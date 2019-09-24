a899490fcbd6db110c21f067006e5db097dac9d6
v-huli <v-huli@HUAPING-THINK>
2019-09-24 00:26:51 -0700

    update

---
+++ JiuZhang.Chapter9/Reverse_Double_Linked_List.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JiuZhang.Chapter9
+{
+    class Reverse_Double_Linked_List
+    {
+        public class Node
+        {
+            public int data;
+            public Node next, previous;
+            public Node(int value)
+            {
+                this.data = value;
+                this.next = this.previous = null;
+            }
+        }
+
+        public void Reverse(Node head)
+        {
+            Node temp = null;
+            Node current = head;
+
+            while(current != null)
+            {
+                temp = current.previous;
+                current.previous = current.next;
+                current.next = temp;
+                current = current.previous;
+            }
+
+            head = temp.previous;
+        }
+
+        public void Push(int new_data, Node head)
+        {
+            Node new_node = new Node(new_data);
+
+            new_node.previous = null;
+            new_node.next = head;
+
+            if (head != null)
+            {
+                head.previous = new_node;
+            }
+
+            head = new_node;
+        }
+    }
+}
---
+++ JiuZhang.Chapter9/StackUsingLinkedList.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JiuZhang.Chapter9
+{
+    class StackUsingLinkedList
+    {
+        private class Node
+        {
+            public int data;
+            public Node link;
+        }
+
+        Node top;
+
+        public StackUsingLinkedList()
+        {
+            this.top = null;
+        }
+
+        public void Push(int value)
+        {
+            Node temp = new Node();
+
+            if (temp == null)
+            {
+                throw new Exception();
+            }
+
+            temp.data = value;
+            temp.link = top;
+
+            top = temp;
+        }
+
+        public bool IsEmpty()
+        {
+            return top == null;
+        }
+
+        public int peek()
+        {
+            if (!IsEmpty())
+            {
+                return top.data;
+            }
+            else
+            {
+                throw new Exception();
+            }
+        }
+
+        public int? Pop()
+        {
+            if (top == null)
+            {
+                return null;
+            }
+            else
+            {
+                top = top.link;
+                return top.data;
+            }
+        }
+    }
+}
