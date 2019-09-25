c2f27720d198a5402589f1a2e3aed5302295c715
v-huli <v-huli@HUAPING-THINK>
2019-09-25 00:07:20 -0700

    update

---
+++ JiuZhang.Chapter7/CoinChange2.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JiuZhang.Chapter7
+{
+    // https://leetcode.com/problems/coin-change-2/
+    // https://www.geeksforgeeks.org/coin-change-dp-7/
+    class CoinChange2
+    {
+        static long countWays(int[] S, int m, int n)
+        {
+            //Time complexity of this function: O(mn) 
+            //Space Complexity of this function: O(n) 
+
+            // table[i] will be storing the number of solutions 
+            // for value i. We need n+1 rows as the table is 
+            // constructed in bottom up manner using the base 
+            // case (n = 0) 
+            int[] table = new int[n + 1];
+
+            // Initialize all table values as 0 
+            for (int i = 0; i < table.Length; i++)
+            {
+                table[i] = 0;
+            }
+
+            // Base case (If given value is 0) 
+            table[0] = 1;
+
+            // Pick all coins one by one and update the table[] 
+            // values after the index greater than or equal to 
+            // the value of the picked coin 
+            for (int i = 0; i < m; i++)
+                for (int j = S[i]; j <= n; j++)
+                    table[j] += table[j - S[i]];
+
+            return table[n];
+        }
+    }
+}
---
+++ JiuZhang.Chapter7/CoinChanges.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JiuZhang.Chapter7
+{
+    // https://leetcode.com/problems/coin-change/
+    class CoinChanges
+    {
+        public int coinChange_topdown(int[] coins, int amount)
+        {
+            if (amount < 1) return 0;
+            return coinChange(coins, amount, new int[amount]);
+        }
+
+        private int coinChange(int[] coins, int rem, int[] count)
+        {
+            if (rem < 0) return -1;
+            if (rem == 0) return 0;
+            if (count[rem - 1] != 0) return count[rem - 1];
+            int min = int.MaxValue;
+            foreach (int coin in coins)
+            {
+                int res = coinChange(coins, rem - coin, count);
+                if (res >= 0 && res < min)
+                    min = 1 + res;
+            }
+            count[rem - 1] = (min == int.MaxValue) ? -1 : min;
+            return count[rem - 1];
+        }
+
+        public int coinChange_BottomUp(int[] coins, int amount)
+        {
+            int max = amount + 1;
+            int[] dp = new int[amount + 1];
+            for (int i = 0; i < amount + 1; i++) dp[i] = int.MaxValue;
+            dp[0] = 0;
+            for (int i = 1; i <= amount; i++)
+            {
+                for (int j = 0; j < coins.Length; j++)
+                {
+                    if (coins[j] <= i)
+                    {
+                        dp[i] = Math.Min(dp[i], dp[i - coins[j]] + 1);
+                    }
+                }
+            }
+            return dp[amount] > amount ? -1 : dp[amount];
+        }
+    }
+}
