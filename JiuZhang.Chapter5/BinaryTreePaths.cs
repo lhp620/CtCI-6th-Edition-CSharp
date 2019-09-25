e9df4059084c1780fdcc5add90b633b3fde747f6
Huaping Liu (CSI Interfusion Inc) <v-huli@microsoft.com>
2019-09-24 23:40:32 -0700

    updates

---
+++ JiuZhang.Chapter5/BinaryTreePaths.cs
@@ -0,0 +1,76 @@
+﻿using ctci.Library;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JiuZhang.Chapter5
+{
+    class Binary_Tree_Paths
+    {
+        public List<string> BinaryTreePaths(TreeNode root)
+        {
+            List<String> paths = new List<string>();
+            if (root == null)
+            {
+                return paths;
+            }
+
+            List<String> leftPaths = BinaryTreePaths(root.Left);
+            List<String> rightPaths = BinaryTreePaths(root.Right);
+            foreach (String path in leftPaths)
+            {
+                paths.Add(root.Data + "->" + path);
+            }
+            foreach (String path in rightPaths)
+            {
+                paths.Add(root.Data + "->" + path);
+            }
+
+            // root is a leaf
+            if (paths.Count == 0)
+            {
+                paths.Add("" + root.Data);
+            }
+
+            return paths;
+        }
+
+
+        public List<String> BinaryTreePaths2(TreeNode root)
+        {
+            List<String> result = new List<string>();
+            if (root == null)
+            {
+                return result;
+            }
+            helper(root, root.Data.ToString(), result);
+            return result;
+        }
+
+        private void helper(TreeNode root, String path, List<String> result)
+        {
+            if (root == null)
+            {
+                return;
+            }
+
+            if (root.Left == null && root.Right == null)
+            {
+                result.Add(path);
+                return;
+            }
+
+            if (root.Left != null)
+            {
+                helper(root.Left, path + "->" + root.Left.Data.ToString(), result);
+            }
+
+            if (root.Right != null)
+            {
+                helper(root.Right, path + "->" + root.Right.Data.ToString(), result);
+            }
+        }
+    }
+}
