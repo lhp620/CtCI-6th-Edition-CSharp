6febefe574107127a8f23188a5553fd7ea24aaf3
v-huli <v-huli@HUAPING-THINK>
2019-09-28 00:19:23 -0700

    update

--- JiuZhang.Chapter6/Permutations.cs
+++ JiuZhang.Chapter6/Permutations.cs
@@ -6,6 +6,20 @@ using System.Threading.Tasks;
 
 namespace JiuZhang.Chapter6
 {
+    /// <summary>
+    /// Given a collection of distinct integers, return all possible permutations.
+    //Example:
+    //Input: [1,2,3]
+    //Output:
+    //[
+    //  [1,2,3],
+    //  [1,3,2],
+    //  [2,1,3],
+    //  [2,3,1],
+    //  [3,1,2],
+    //  [3,2,1]
+    //]
+    /// </summary>
     class Permutations
     {
         public List<List<int>> Permutations_(int[] nums, int n)
